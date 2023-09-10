84082e559345bdbabd1c0ec03b7ca8878987fb10
deepakjain124 <deepakjaindj@gmail.com>
2023-09-10 09:50:11 +0530

    change origin

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://gptoutputdetector.com",
+    origin: "https://gptoutputdetector.com",
     // origin:"http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
