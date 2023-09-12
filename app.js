1ff8473b262b1c235b8f5c1ad001665a6bf09309
deepakjain124 <deepakjaindj@gmail.com>
2023-09-12 23:47:11 +0530

    api changes

--- app.js
+++ app.js
@@ -47,6 +47,39 @@ app.post("/api/third-party-endpoint/detectText", async (req, res) => {
     res.status(500).send(error);
   }
 });
+
+app.post("/api/third-party-endpoint/detectImage", async (req, res) => {
+  try {
+    var config = {
+      method: "post",
+      url: "https://real-or-fake-image.p.rapidapi.com/api-v1.0/SafeUnsafeImageWithTags",
+      headers: {
+        "content-type": "application/json",
+        "X-RapidAPI-Key": "90d7afdba5msh12be53e53b2d01fp17a0bbjsn282e40360e36",
+        "X-RapidAPI-Host": "real-or-fake-image.p.rapidapi.com",
+      },
+      data: {
+        api_key: 'test_fake_image_rapid',
+        base64_image: req.body.base64_image,
+      },
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(response)
+        res.send(response.data);
+      })
+      .catch(function (error) {
+        console.log(error)
+        res.send(error);
+      });
+    // Forward the response to the client
+  } catch (error) {
+    // Handle errors appropriately
+    console.error(error);
+    res.status(500).send(error);
+  }
+});
 app.listen(port, () => {
   console.log(`Proxy server is running on port ${port}`);
 });
