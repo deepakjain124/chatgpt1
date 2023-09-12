32cd98d6e36eeb183527b3f0effe81ff05bd9997
deepakjain124 <deepakjaindj@gmail.com>
2023-09-12 20:52:56 +0530

    api changes

--- app.js
+++ app.js
@@ -10,41 +10,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["https://gptoutputdetector.com","https://gptzero.com"],
+    origin: [
+      "https://gptoutputdetector.com",
+      "https://gptzero.com",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
 );
-app.post("/api/third-party-endpoint/login", async (req, res) => {
-  try {
-    const response = await axios.post(
-      "https://api.zerogpt.com/api/auth/login",
-      {
-        email: "deepakjaindj052@gmail.com",
-        password: "123456abc",
-      }
-    );
-    // Forward the response to the client
-    res.json(response.data);
-  } catch (error) {
-    // Handle errors appropriately
-    res.status(500).send(error);
-  }
-});
 
 app.post("/api/third-party-endpoint/detectText", async (req, res) => {
-  console.log(req)
   try {
     var config = {
       method: "post",
-      url: "https://api.zerogpt.com/api/detect/detectText",
+      url: "https://zerogpt.p.rapidapi.com/api/v1/detectText",
       headers: {
-        "Content-Type": "application/json",
-        Authorization:
-          `Bearer ${req.body.token}`,
-          "ApiKey":"5518bf40-af89-424e-833c-3c2cac3f9437"
+        "content-type": "application/json",
+        "X-RapidAPI-Key": "90d7afdba5msh12be53e53b2d01fp17a0bbjsn282e40360e36",
+        "X-RapidAPI-Host": "zerogpt.p.rapidapi.com",
       },
       data: {
-        input_text:req.body.input_text
+        input_text: req.body.input_text,
       },
     };
 
@@ -53,7 +38,7 @@ app.post("/api/third-party-endpoint/detectText", async (req, res) => {
         res.send(response.data);
       })
       .catch(function (error) {
-        res.send(error)
+        res.send(error);
       });
     // Forward the response to the client
   } catch (error) {
